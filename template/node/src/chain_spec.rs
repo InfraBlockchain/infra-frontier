b72b5b4c400fd54f38eff9da3bd4ca9818e61528
Cute_Wisp <sweatpotato13@gmail.com>
2023-07-26 09:17:59 +0900

    feat: 🎸 add asset_id in evm genesis config

--- template/node/src/chain_spec.rs
+++ template/node/src/chain_spec.rs
@@ -232,6 +232,7 @@ fn testnet_genesis(
 		// EVM compatibility
 		evm_chain_id: EVMChainIdConfig { chain_id },
 		evm: EVMConfig {
+			asset_id: 99,
 			accounts: {
 				let mut map = BTreeMap::new();
 				map.insert(
